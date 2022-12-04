1244e87479a8ba07ee1245ead2ace248552150a8
Alexander Flex <ronion@yandex.ru>
2022-12-04 11:54:19 +0300

    Tests for one and no one cases added

--- src/test/java/ProductManagerTest.java
+++ src/test/java/ProductManagerTest.java
@@ -1,11 +1,10 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ProductManagerTest {
     ProductRepository repo = new ProductRepository();
     ProductManager manager = new ProductManager(repo);
-
-
     Product item1 = new Product(1,"Neznayka in Solar Sity", 200);
     Product item2 = new Product(12,"Neznayka on the Moon", 200);
     Product item3 = new Product(23,"Readme 4x", 10000);
@@ -13,40 +12,48 @@ public class ProductManagerTest {
     Product item5 = new Product(42,"War and peace", 500);
     Product item6 = new Product(54,"Realme 8 pro", 25000);
 
-//    @BeforeEach
-//    public void setup(){
-//        manager.add(item1);
-//        manager.add(item2);
-//        manager.add(item3);
-//        manager.add(item4);
-//        manager.add(item5);
-//    }
-    /// Непонятно почему не работает добавление через @BeforeEach
-
-    @Test
-    public void testAdd(){
+    @BeforeEach
+    public void setup(){
         manager.add(item1);
         manager.add(item2);
         manager.add(item3);
         manager.add(item4);
         manager.add(item5);
+        manager.add(item6);
+    }
+    /// Непонятно почему не работает добавление через @BeforeEach
 
-        Product[] expected = {item1, item2, item3, item4, item5};
+    @Test
+    public void testAdd(){
+
+        Product[] expected = {item1, item2, item3, item4, item5, item6};
         Product[] actual = repo.getItems();
 
         Assertions.assertArrayEquals(expected,actual);
     }
 
     @Test
-    public void testSearh(){
-        manager.add(item1);
-        manager.add(item2);
-        manager.add(item3);
-        manager.add(item4);
-        manager.add(item5);
+    public void testSearhSeveral(){
 
         Product[] expected = {item1, item2};
         Product[] actual = manager.searchBy("Neznayka");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testSearhOne() {
+
+        Product[] expected = {item3};
+        Product[] actual = manager.searchBy("Readme");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSearhNoOne() {
+
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("Earth");
 
         Assertions.assertArrayEquals(expected, actual);
     }
